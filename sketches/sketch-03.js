e394024205ac4c9fbad3ae8508c90e1cd3127600
Richie Wallett <richgm@gmail.com>
2021-12-08 13:48:55 +0000

    Updates

---
+++ sketches/sketch-03.js
@@ -0,0 +1,63 @@
+// Source t.ly/Lt4y
+
+const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = ({ context, width, height }) => {
+
+  const agents = [];
+  
+  for (let i = 0; 1 < 40; i++) {
+    const x = random.range(0, width);
+    const y = random.range(0, height);
+
+    agents.push(new Agent(x, y));
+  }
+
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    agents.forEach(agent => {
+      agent.draw(context);
+    });
+
+    // SHF ALT A to comment our selection
+/*     // Instances of the class
+    const agentA = new Agent(800, 400);
+    const agentB = new Agent(300, 700);
+
+    agentA.draw(context);
+    agentB.draw(context); */
+
+  };
+};
+
+canvasSketch(sketch, settings);
+
+// Classes
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+    this.pos = new Point(x, y);
+    this.radius = 10;
+  }
+
+  draw(context) {
+    context.fillStyle = 'black';
+
+    context.beginPath();
+    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+  }
+}
